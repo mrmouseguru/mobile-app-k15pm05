3c127f16cef860f61ddfcb2d93e7e2140ae954dd
CGM <mrmouse.guru@gmail.com>
2024-03-04 13:10:52 +0700

    initialize

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hello-k15pm05-mobile-app"
+include(":app")

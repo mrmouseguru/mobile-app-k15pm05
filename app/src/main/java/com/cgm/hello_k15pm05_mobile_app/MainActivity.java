e61f63ff31d24aca35fb1846ae440e79fbfd5f65
CGM <mrmouse.guru@gmail.com>
2024-03-04 14:03:54 +0700

    ListView showing Product list data

--- app/src/main/java/com/cgm/hello_k15pm05_mobile_app/MainActivity.java
+++ app/src/main/java/com/cgm/hello_k15pm05_mobile_app/MainActivity.java
@@ -3,6 +3,13 @@ package com.cgm.hello_k15pm05_mobile_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.cgm.hello_k15pm05_mobile_app.entities.Product;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView productListView = findViewById(R.id.productList);
+
+        List<Product> productListData = Arrays.asList(
+                new Product("Sam sung galaxy", 700, "p1.jpg"),
+                new Product("Iphone 15 pro max", 1200, "p2.jpg")
+        );
+        ArrayAdapter<Product> adapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_list_item_1, productListData);
+        productListView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/cgm/hello_k15pm05_mobile_app/entities/Product.java
@@ -0,0 +1,48 @@
+package com.cgm.hello_k15pm05_mobile_app.entities;
+
+public class Product {
+    private String name;
+    private double price;
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, String image) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
